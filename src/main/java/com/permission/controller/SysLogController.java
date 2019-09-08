be63ce4b728fcdec4bb72cd9ed7fc82ed8560157
yami <yami19931023@163.com>
2019-09-08 22:44:16 +0800

    增加insert主键返回

--- src/main/java/com/permission/controller/SysLogController.java
+++ src/main/java/com/permission/controller/SysLogController.java
@@ -18,11 +18,20 @@ public class SysLogController {
     @Autowired
     private SysLogService sysLogService;
 
+    /**
+     * 进入日志页面
+     * @return
+     */
     @RequestMapping("/log.page")
     public ModelAndView page() {
         return new ModelAndView("log");
     }
 
+    /**
+     * 根据操作日志还原
+     * @param id
+     * @return
+     */
     @RequestMapping("/recover.json")
     @ResponseBody
     public JsonData recover(@RequestParam("id") int id) {
@@ -30,6 +39,12 @@ public class SysLogController {
         return JsonData.success();
     }
 
+    /**
+     * 分页查询操作日志
+     * @param param
+     * @param page
+     * @return
+     */
     @RequestMapping("/page.json")
     @ResponseBody
     public JsonData searchPage(SearchLogParam param, PageQuery page) {
